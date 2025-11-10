67de44fffe251d06785d3b608851aaa9bf582d3b
krithikapoojary2424 <krithikapoojary2424@gmail.com>
2025-11-10 09:09:49 +0530

    Fourth commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html')  # assumes templates/index.html exists
 
-if __name__== "__main__":
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run()
